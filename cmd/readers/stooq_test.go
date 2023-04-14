111aa885bdee6c9b959e503d5afeab937a06e5f1
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-04-14 18:56:51 +0200

    add test for getParams method

--- cmd/readers/stooq_test.go
+++ cmd/readers/stooq_test.go
@@ -1,6 +1,8 @@
 package reader
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -15,5 +17,31 @@ func TestNewStooqDataReader(t *testing.T) {
 	stooqUrl := BaseUrlMap["stooq"]
 	if stooqReader.baseUrl != stooqUrl {
 		t.Errorf("FAIL: expected %s, got %s", stooqUrl, stooqReader.baseUrl)
+	}
+}
+
+func TestGetParams(t *testing.T) {
+	d1 := time.Now().AddDate(0, 0, -1)
+	d2 := time.Now()
+	stooqReader, err := NewStooqDataReader([]string{"PKO"}, d1, d2, "d")
+
+	if err != nil {
+		t.Errorf("FAIL: %s", err)
+	}
+
+	expectedParams := map[string]string {
+		"s": "PKO",
+		"i": "d",
+		"d1": d1.Format("2006-01-02"),
+		"d2": d2.Format("2006-01-02"),
+	}
+	params := stooqReader.getParams("PKO")
+
+	cmp := reflect.DeepEqual(params, expectedParams)
+
+	if !cmp {
+		fmt.Println("Expected: ", expectedParams)
+		fmt.Println("Got: ", params)
+		t.Error("Params don't match")
 	}
 }
