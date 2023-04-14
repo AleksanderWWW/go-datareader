072fcb31ec741d5bdd7d50d23a75c60a72bfcd05
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-04-14 12:38:07 +0200

    add test for NewStooqDataReaderMethod

---
+++ cmd/readers/stooq_test.go
@@ -0,0 +1,19 @@
+package reader
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNewStooqDataReader(t *testing.T) {
+	stooqReader, err := NewStooqDataReader(time.Now().AddDate(0, 0, -1), time.Now())
+
+	if err != nil {
+		t.Errorf("FAIL: %s", err)
+	}
+
+	stooqUrl := BaseUrlMap["stooq"]
+	if stooqReader.baseUrl != stooqUrl {
+		t.Errorf("FAIL: expected %s, got %s", stooqUrl, stooqReader.baseUrl)
+	}
+}
