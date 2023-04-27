9d94d958a094b34250ddeaa3ed0a8acfd02ba624
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-04-27 17:12:17 +0200

    remove unused function - parsing stooq line

--- reader/utils.go
+++ reader/utils.go
@@ -5,49 +5,10 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/go-gota/gota/series"
 )
-
-func parseStooqLine(line string, symbol string) (SingleRecord, error) {
-	items := strings.Split(line, ",")
-
-	open, err := strconv.ParseFloat(items[1], 32)
-	if err != nil {
-		return SingleRecord{}, err
-	}
-
-	high, err := strconv.ParseFloat(items[2], 32)
-	if err != nil {
-		return SingleRecord{}, err
-	}
-
-	low, err := strconv.ParseFloat(items[3], 32)
-	if err != nil {
-		return SingleRecord{}, err
-	}
-
-	close, err := strconv.ParseFloat(items[4], 32)
-	if err != nil {
-		return SingleRecord{}, err
-	}
-	volume, err := strconv.ParseInt(strings.Replace(items[5], "\r", "", 1), 10, 32)
-	if err != nil {
-		return SingleRecord{}, err
-	}
-	return SingleRecord{
-		Symbol: symbol,
-		Date:   items[0],
-		Open:   open,
-		High:   high,
-		Low:    low,
-		Close:  close,
-		Volume: int(volume),
-	}, nil
-}
 
 func createRequest(params map[string]string, headers map[string]string, baseUrl string) (*http.Request, error) {
 	parameters := url.Values{}
