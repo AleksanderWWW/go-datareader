385c154e7a08803f4a6be80b9cb1e10736103103
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-05-21 17:33:25 +0200

    test call count of methods

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -8,20 +8,24 @@ import (
 	"github.com/go-gota/gota/series"
 )
 
-type MockReader struct{}
+type MockReader struct {
+	readSingleCallCount       int
+	concatDataframesCallCount int
+}
 
 func (mr MockReader) getSymbols() []string {
 	return []string{"testSymbol1", "testSymbol2", "testSymbol3"}
 }
 
-func (mr MockReader) readSingle(symbol string) (dataframe.DataFrame, error) {
+func (mr *MockReader) readSingle(symbol string) (dataframe.DataFrame, error) {
+	mr.readSingleCallCount += 1
 	return dataframe.New(
 		series.New([]int{1, 2}, series.Int, "Index"),
 		series.New([]string{"b", "a"}, series.String, symbol),
 	), nil
 }
 
-func (mr MockReader) concatDataframes(dfs []dataframe.DataFrame) dataframe.DataFrame {
+func (mr *MockReader) concatDataframes(dfs []dataframe.DataFrame) dataframe.DataFrame {
 	combined := dfs[0]
 
 	for _, df := range dfs[1:] {
@@ -30,6 +34,8 @@ func (mr MockReader) concatDataframes(dfs []dataframe.DataFrame) dataframe.DataF
 
 	// to assert stable order of columns
 	combined = combined.Select([]string{"Index", "testSymbol1", "testSymbol2", "testSymbol3"})
+
+	mr.concatDataframesCallCount += 1
 	return combined
 }
 
@@ -43,7 +49,7 @@ func TestGetData(t *testing.T) {
 		series.New([]string{"b", "a"}, series.String, "testSymbol3"),
 	)
 
-	obtained := GetData(mockReader)
+	obtained := GetData(&mockReader)
 
 	if !reflect.DeepEqual(expected.Records(), obtained.Records()) {
 		t.Errorf("Different values:\nExpected:%v\nObtained:%v", expected.Records(), obtained.Records())
@@ -51,5 +57,13 @@ func TestGetData(t *testing.T) {
 
 	if !reflect.DeepEqual(expected.Types(), obtained.Types()) {
 		t.Errorf("Different types:\nExpected:%v\nObtained:%v", expected.Types(), obtained.Types())
+	}
+
+	if mockReader.readSingleCallCount != 3 {
+		t.Errorf("Expected 'readSingle' to be called 3 times. Called %d times", mockReader.readSingleCallCount)
+	}
+
+	if mockReader.concatDataframesCallCount != 1 {
+		t.Errorf("Expected 'concatDataframed' to be called once. Called %d times", mockReader.concatDataframesCallCount)
 	}
 }
