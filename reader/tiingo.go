0d7257bd7f2e19975cf40397089f52b54209acb0
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-13 22:02:28 +0100

    fix typo in license header (#7)

--- reader/tiingo.go
+++ reader/tiingo.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 Aleksander WOjnarowicz <alwojnarowicz@gmail.com>
+Copyright © 2023 Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
