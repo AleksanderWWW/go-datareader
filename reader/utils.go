00408b37e8dfa1125a34e629b1f2c3471af58b52
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-14 18:59:30 +0100

    update utils

--- reader/utils.go
+++ reader/utils.go
@@ -108,9 +108,3 @@ func renameDataframe(df dataframe.DataFrame, symbol string) dataframe.DataFrame 
 	}
 	return df
 }
-
-func getLoggerName(name string) string {
-	year, month, day := time.Now().Date()
-
-	return fmt.Sprintf("%v-%v-%v-%v.log", name, day, month.String(), year)
-}
